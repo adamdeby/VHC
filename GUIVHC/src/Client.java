db7e40f415f0acf5eac179f154a1ded96538dc74
Patryk-Perkowski <Patryk.Perkowski19@my.stjohns.edu>
2022-10-27 23:45:41 -0400

    Client.java and Owner.java added

--- GUIVHC/src/Client.java
+++ GUIVHC/src/Client.java
@@ -1,4 +1,49 @@
 
 public class Client {
 
+	private String firstName;
+	private String lastName;
+	private String iD;
+	
+	public Client(String firstName, String lastName, String iD) 
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.iD = iD;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getiD() {
+		return iD;
+	}
+
+	public void setiD(String iD) {
+		this.iD = iD;
+	}
+	
+
+	void createJob() {
+		//
+	}
+	
+
+	void submitJob() {
+		//
+	}
+	
 }
--- GUIVHC/src/Owner.java
+++ GUIVHC/src/Owner.java
@@ -1,4 +1,58 @@
 
 public class Owner {
 
+	private String iD;
+	private String firstName;
+	private String lastName;
+	private int arrivalTime;
+	private int departureTime;
+	
+	public Owner(String iD,String firstName, String lastName, int arrivalTime,int departureTime) 
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.iD = iD;
+		this.arrivalTime = arrivalTime;
+		this.departureTime = departureTime;
+	}
+	
+	
+	public String getiD() {
+		return iD;
+	}
+	public void setiD(String iD) {
+		this.iD = iD;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getArrivalTime() {
+		return arrivalTime;
+	}
+	public void setArrivalTime(int arrivalTime) {
+		this.arrivalTime = arrivalTime;
+	}
+	public int getDepartureTime() {
+		return departureTime;
+	}
+	public void setDepartureTime(int departureTime) {
+		this.departureTime = departureTime;
+	}
+
+	
+	void addVeichle() {
+		//
+	}
+
+	
+	
 }
