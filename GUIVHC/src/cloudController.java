ae57a8706f01357935383159076d91c70ec01401
adamdeby <adamddddd1@gmail.com>
2022-11-12 01:36:17 -0500

    added socket programming logic and barebone functions.

--- GUIVHC/src/cloudController.java
+++ GUIVHC/src/cloudController.java
@@ -3,28 +3,172 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.*;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
 
 
-
 public class cloudController extends ClientGui {
-	
-	public static ArrayList<Integer> computeResult(ArrayList<Integer>input){
-		int sum=0;
-		ArrayList<Integer> resultArray = new ArrayList<Integer>() ;
-		for(int i=0;i<input.size();i++)
-		{
+
+	public static ArrayList<Integer> computeResult(ArrayList<Integer> input) {
+		int sum = 0;
+		ArrayList<Integer> resultArray = new ArrayList<Integer>();
+		for (int i = 0; i < input.size(); i++) {
 			sum += input.get(i);
 			resultArray.add(sum);
-			
+
 		}
 		return resultArray;
 	}
 
+	static ServerSocket serverSocket;
+	static Socket socket;
+	static DataInputStream inputStream;
+	static DataOutputStream outputStream;
 
+	public static void main(String[] args) {
+
+		String dataIn = "";
+		try {
+
+			System.out.println("----------$$$ This is server side $$$--------");
+			System.out.println("wating for client to connect...");
+			// creating the server
+			serverSocket = new ServerSocket(3322);
+			// sever accepts connection request from client
+			socket = serverSocket.accept();
+			System.out.println("client is connected!");
+			System.out.println("go to client side and send me a message");
+
+			inputStream = new DataInputStream(socket.getInputStream());
+			outputStream = new DataOutputStream(socket.getOutputStream());
+
+			class acceptRejectWindow extends JFrame {
+				final int FrameWidth = 550;
+				final int FrameHeight = 400;
+				JLabel question;
+				JButton acceptButton;
+				JButton rejectButton;
+
+				public acceptRejectWindow() {
+					question = new JLabel("Do you want to accept or reject the client's data?");
+					createAcceptButton();
+					 createRejectButton();
+					 createPanel();
+
+					setSize(FrameWidth, FrameHeight);
+				}
+
+				private void createAcceptButton() {
+					acceptButton = new JButton("Accept");
+					acceptButton.setBounds(10, 100, 180, 125);
+					ActionListener acceptButtonListener = new acceptButtonListener();
+					acceptButton.addActionListener(acceptButtonListener);
+				}
+
+				class acceptButtonListener implements ActionListener {
+
+					public void actionPerformed(ActionEvent e) {
+						if (e.getActionCommand() == acceptButton.getActionCommand()) {
+
+							try {
+								////
+								// clientTable();
+								// vehicleOwnerTable();
+								// ////
+
+								PrintStream output = new PrintStream(new FileOutputStream("Client Data.txt", true));
+								output.println("**************************************");
+								output.println("CLIENT DATA: ");
+								output.println(inputStream);
+								output.println("**************************************");
+
+								outputStream.writeUTF("DATA ACCEPTED");
+								output.close();
+							} catch (Exception a) {
+
+								a.printStackTrace();
+							}
+						}
+
+					}
+
+				}
+				class rejectButtonListener implements ActionListener
+				{
+					public void actionPerformed(ActionEvent e)
+					{	
+						if(e.getActionCommand()== rejectButton.getActionCommand())
+						{
+							try 
+							{
+								outputStream.writeUTF("DATA REJECTED");
+							}
+							catch(Exception ex){JOptionPane.showMessageDialog(null, ex);} 
+							{
+								
+							}
+						}
+					}
+				}
+
+				private void createRejectButton() 
+				{
+					rejectButton = new JButton("Reject");
+					rejectButton.setBounds(10, 125, 80, 25);
+					ActionListener rejectButtonListener = new rejectButtonListener(); 
+					rejectButton.addActionListener(rejectButtonListener);
+				}
+				private void createPanel() 
+				{
+					JPanel panel = new JPanel();
+					panel.add(question);
+					panel.add(acceptButton);
+					panel.add(rejectButton);
+					add(panel);
+				}
+					
+			}
+			//////
+			
+			while(true)
+			{
+				if(dataIn.equals("exit")==true) 
+				{
+					System.exit(1);
+				}
+				
+				dataIn = inputStream.readUTF();
+				System.out.println("Data received from client: " + "\"" + dataIn + "\"");
+				
+				MainMenu mFrame = new MainMenu();
+				
+				mFrame.Mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				mFrame.Mainframe.setVisible(true);
+				
+				JFrame frame = new acceptRejectWindow();
+				frame.setTitle("SERVER");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+
+			}
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+	}
 }
-
-
---
+++ GUIVHC/src/networkClient.java
@@ -0,0 +1,62 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class networkClient {
+	
+	 static ServerSocket serverSocket;
+		static Socket socket;
+		static DataInputStream inputStream;
+		static DataOutputStream outputStream;
+
+
+	public static void main(String[] args) {
+		
+		String dataIn = "";
+		String dataOut = "";
+		Scanner input;
+		
+		try 
+		
+		{
+			 System.out.println("******** Client Side ********");
+			 System.out.println("Client Sucessfully Connected!... ");
+			
+			Socket socket = new Socket("localhost", 3322);
+			
+			inputStream = new DataInputStream(socket.getInputStream());
+			outputStream = new DataOutputStream(socket.getOutputStream());
+			System.out.println("Enter Yes if you have data you want to send to the server.");
+			System.out.println("Otherwise type exit.");
+			input = new Scanner(System.in);
+			dataOut = input.nextLine();
+			outputStream.writeUTF(dataOut);
+			
+			while(true)
+			{
+				if(dataIn.equals("exit")) 
+				{
+					socket.close();
+					System.out.println("GOODBYE");
+					System.exit(1);
+				}
+				dataIn = inputStream.readUTF();
+				System.out.println("message received from server: " + "\"" + dataIn + "\"");
+				
+				System.out.println("Enter Yes if you have data you want to send to the server.");
+				System.out.println("Otherwise type exit.");
+				input = new Scanner(System.in);
+				dataOut = input.nextLine();
+				outputStream.writeUTF(dataOut);
+			}
+			
+			
+	}
+		catch(Exception e) 
+		{
+			e.printStackTrace();
+		}
+}
+}
