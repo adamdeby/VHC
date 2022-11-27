e897978eb824926c9ae8cd445459b783d5a0ea8a
Patryk-Perkowski <Patryk.Perkowski19@my.stjohns.edu>
2022-11-27 15:29:16 -0500

    modified the buttons to move server into controller gui

--- GUIVHC/src/CloudControllerGui.java
+++ GUIVHC/src/CloudControllerGui.java
@@ -39,7 +39,7 @@ public class CloudControllerGui extends cloudController {
 	private JButton Refresh;
 	private final Action action_1 = new SwingAction_1();
 	/**
-	 * Launch the application.
+	 * Launch the application.//
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
